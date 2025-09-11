4c0e2d040a170541e6ed102cac8390712adf52f8
Kavinda <kavinda.appuhamy@gmail.com>
2025-09-11 23:15:07 +0530

    Added App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
+import Portfolio from './pages/Portfolio'
 
 function App() {
 
   return (
     <>
-      
+      <BrowserRouter>
+        <div>  
+          <Routes>
+            <Route path="/*" element={<Portfolio />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   )
 }
